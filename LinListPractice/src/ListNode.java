e521a9d909f75456a346d74157a3342107e9be70
Fengxiang Xue <47923440+frankxue831@users.noreply.github.com>
2020-12-19 20:27:45 +0800

    add hasCycle method

---
+++ LinListPractice/src/ListNode.java
@@ -0,0 +1,52 @@
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int num){
+        val = num;
+        next = null;
+    }
+
+
+    public int getVal() {
+        return val;
+    }
+
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        if(head == null){
+            return false;
+        }
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static void main(String[]  args){
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        b.next = a;
+        System.out.println(b.toString());
+    }
+}
