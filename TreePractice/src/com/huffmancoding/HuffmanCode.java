c559da7ba4f59bb64f5a00499425d2f8085ba8c7
Fengxiang Xue <47923440+frankxue831@users.noreply.github.com>
2021-03-04 12:12:34 +0800

    adding HuffmanCode for coding string

--- TreePractice/src/com/huffmancoding/HuffmanCode.java
+++ TreePractice/src/com/huffmancoding/HuffmanCode.java
@@ -2,7 +2,17 @@ package com.huffmancoding;
 
 import java.util.*;
 
+/**
+ *
+ */
 public class HuffmanCode {
+
+    // Using map to store the huffman code
+    static Map<Byte, String> huffmanCodes = new HashMap<Byte, String>();
+    // Using StringBuilder to save Huffman code representation
+    static StringBuilder sb = new StringBuilder();
+
+
     public static void main(String[] args){
         String str = "I like like like java do you like a java";
         byte[] contentBytes = str.getBytes();
@@ -15,11 +25,27 @@ public class HuffmanCode {
         System.out.println("Huffman Tree: \n");
         Node huffmanTreeRoot = createHuffmanTree(nodes);
         System.out.println("PreOrder form:");
-        huffmanTreeRoot.preOrder();
+        HuffmanCode.preOrder(huffmanTreeRoot);
+
+    }
+
+    // generate the Huffman code by Huffman tree, every left path mark as 0, right as 1
+
+    /**
+     * get all leaf node of the passing node as Huffman code, and put them in huffmanCodes map;
+     * @param node the passing node
+     * @param code left node mark as 0, right node mark as 1
+     * @param stringBuilder collect the path of node. Example:
+     *                     [left, right, left]
+     *                      ->[0,1,0]
+     *                      ->010
+     */
+    private static void getCodes(Node node, String code, StringBuilder stringBuilder){
+
     }
 
     /**
-     *
+     * create a list of nodes from the giving byte array
      * @param bytes the bytes from the string
      * @return The list of nodes for counting each character repeated times
      */
@@ -28,13 +54,7 @@ public class HuffmanCode {
 
         Map<Byte, Integer> counts = new HashMap<>();
         for(byte b : bytes) {
-            Integer count = counts.get(b);
-            if(count == null){
-                counts.put(b, 1);
-
-            } else {
-                counts.put(b, count + 1);
-            }
+            counts.merge(b, 1, Integer::sum);
         }
 
         for(Map.Entry<Byte, Integer> entry: counts.entrySet()) {
@@ -45,25 +65,40 @@ public class HuffmanCode {
 
     }
 
+    /**
+     * create the Huffman tree from the giving list of nodes
+     * @param nodes the list of nodes which contain value and weights
+     * @return root as Node, which is the root of Huffman tree
+     */
     private static Node createHuffmanTree(List<Node> nodes){
         while (nodes.size() > 1){
+            // 1. sort the order of weights
             Collections.sort(nodes);
+            // 2.get the least weight nodes as children
             Node leftNode = nodes.get(0);
             Node rightNode = nodes.get(1);
+            //3. create the subtree for above two children nodes
             Node parent = new Node(null, leftNode.weight + rightNode.weight);
             parent.left = leftNode;
             parent.right = rightNode;
+            //4. remove the assigned nodes from list
             nodes.remove(leftNode);
             nodes.remove(rightNode);
+            //5. add the new subtree to the tree
             nodes.add(parent);
             //System.out.println("###");
         }
 
+        //when left the last node, mark it as root of tree
         //return the root of HuffmanTree
         return nodes.get(0);
     }
 
-    private static void preOrder(Node root){
+    /**
+     * Traverse the tree with preorder
+     * @param root Node the root of the tree
+     */
+    public static void preOrder(Node root){
         if(root != null){
             root.preOrder();
         } else{
