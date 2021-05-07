9f9c421d7989b74019fb7a2642e5cea2306d4a29
Fengxiang Xue <47923440+frankxue831@users.noreply.github.com>
2021-05-07 15:32:39 +0800

    add second solution for LongestPalindrome Problem add solution for ContainerWithMostWater Problem add solution for addTwoNumbers Problem add solution for RomanNumber to Modern Number Problem add solution for Modern Number to RomanNumber Problem

--- OthersPractice/src/RomanNumber.java
+++ OthersPractice/src/RomanNumber.java
@@ -3,6 +3,8 @@
  */
 public class RomanNumber {
     public static void main(String[] args){
+        System.out.println(romanToInt("IV"));
+        System.out.println(intToRoman(671));
 
     }
 
@@ -23,6 +25,21 @@ public class RomanNumber {
         return sum;
     }
 
+    public static String intToRoman(int num){
+        int[] values = {1000, 900, 500,400,100,90,50,40,10,9,5,4,1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder cur = new StringBuilder();
+        for(int i = 0; i< values.length && num >=0; i++){
+            while(values[i] <= num){
+                num -=values[i];
+                cur.append(symbols[i]);
+            }
+        }
+
+        return cur.toString();
+
+    }
+
     private static int getValue(char ch){
         switch(ch) {
             case 'I': return 1;
@@ -35,4 +52,5 @@ public class RomanNumber {
             default: return 0;
         }
     }
+
 }
